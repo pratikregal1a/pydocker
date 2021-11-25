6f019bcb1907d0d15a10430110d442b42d6afa7d
pratikregal1a <57472214+pratikregal1a@users.noreply.github.com>
2021-11-25 15:22:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
             "<img src="C:\Users\Pratik Regal\Desktop\bournvita.png" alt="W3Schools.com" width="104" height="142">"
     return html
 
-PORT = int(os.environ.get("PORT", 8080))
+PORT = int(os.environ.get("PORT", 9090))
 if __name__ == '__main__':
     app.run(threaded=True,host='0.0.0.0',port=PORT)
 
