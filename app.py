2f22efc47753b0d1fb906692da313dd81a3554f2
pratikregal1a <57472214+pratikregal1a@users.noreply.github.com>
2021-11-25 11:46:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello, World!</h3>"
+    html = "<h2>HTML Images</h2>"
+            "<p>HTML images are defined with the img tag:</p>"
+
+            "<img src="C:\Users\Pratik Regal\Desktop\bournvita.png" alt="W3Schools.com" width="104" height="142">"
     return html
 
 if __name__ == "__main__":
